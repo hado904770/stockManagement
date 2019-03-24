28a04b380b5aa2737b49e5925f63841de0622cf4
vancong <51200398cong@gmail.com>
2019-03-24 21:38:26 +0700

    init project

--- src/main/java/com/doan/stockmanagement/controller/HelloController.java
+++ src/main/java/com/doan/stockmanagement/controller/HelloController.java
@@ -1,19 +1,14 @@
 package com.doan.stockmanagement.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 	
-	@RequestMapping(value = "/hello", method = RequestMethod.GET)
-	@ResponseBody
-	public String hello(HttpServletRequest request ) {
-		return "<h1>" + "anc" + "</h1>";
+	@RequestMapping(value = "/")
+	public String hello() {
+		return "home";
 	}
 	
 }
