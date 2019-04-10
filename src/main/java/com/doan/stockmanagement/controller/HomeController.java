edfca9911afb4da417ce79f7e563d3d84d12988e
vancong <51200398cong@gmail.com>
2019-04-10 07:46:40 +0700

    update

--- src/main/java/com/doan/stockmanagement/controller/HomeController.java
+++ src/main/java/com/doan/stockmanagement/controller/HomeController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HelloController {
+public class HomeController {
 	
-	@RequestMapping(value = "/")
+	@RequestMapping
 	public String hello() {
-		return "home";
+		return "pages/index";
 	}
 	
 }
