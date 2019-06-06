e1edec539266ce6ca99ab9417d523b4e1880069f
vancong <51200398cong@gmail.com>
2019-06-06 22:17:36 +0700

    add code

--- src/main/resources/static/js/goods_receipt_note.js
+++ src/main/resources/static/js/goods_receipt_note.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $(function() {
     blockInsertGoodsReceiptNode();
+    blockSearchInsertGoodsReceiptNode();
     insertGoodsReceiptNode();
   });
 });
@@ -79,6 +80,76 @@ function blockInsertGoodsReceiptNode() {
 
       e.stopPropagation();
     });
+  });
+}
+
+function blockSearchInsertGoodsReceiptNode() {
+  let searchInsert = $(`#add_skill_product`);
+  searchInsert.find(`#search_warehouse_btn`).keypress(function(e) {
+    console.log("hello hello");
+    searchInsert
+      .find(`#danger_search_insert`)
+      .removeClass(`d-block`)
+      .addClass(`d-none`)
+      .html(``);
+
+    let keycode = e.keyCode ? e.keyCode : e.which;
+    let elLiveSearch = searchInsert.find(`#live_search`);
+
+    if (keycode == `13`) {
+      let elSearch = elInsert.find(`#search_warehouse_btn`);
+      let warehouse = elSearch.val();
+
+      alert("hello: ", keycode);
+
+      $.ajax({
+        type: `POST`,
+        url: DOMAIN_API + DOMAIN_WAREHOUSE + REQUEST_FILTER,
+        contentType: `application/json`,
+        dataType: `json`,
+        data: JSON.stringify({
+          name: warehouse
+        }),
+        beforeSend: function() {
+          elLiveSearch.html(LOAD_WAITING);
+        },
+        success: function(res) {
+          console.log(res);
+          elLiveSearch.html("");
+          // elLiveSearch.html(
+          //   Mustache.to_html($(`#tplSearchProducts`).html(), res)
+          // );
+
+          searchInsert
+            .find(`#danger_search_insert`)
+            .removeClass(`d-block`)
+            .addClass(`d-none`)
+            .html(``);
+          searchInsert.val("");
+        },
+        error: function(e) {
+          elLiveSearch.html("");
+          let error = `<div class='alert alert-danger'>`;
+          error += `<strong>Failure!</strong> The search skill is the failure!.<br>`;
+          error += `<div class="text-danger">`;
+          error = `<h4> Status error: ` + e.status + `</h4>`;
+          error += e.responseText;
+          error += `</div></div>`;
+
+          searchInsert
+            .find(`#danger_search_insert`)
+            .removeClass(`d-none`)
+            .addClass(`d-block`)
+            .html(error);
+        }
+      });
+
+      e.preventDefault();
+    } else {
+      elLiveSearch.html(``);
+    }
+
+    e.stopPropagation();
   });
 }
 
